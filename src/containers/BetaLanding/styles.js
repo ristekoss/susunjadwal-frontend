87ecd837ad2b1c6efefa874f6d87ce53321268b3
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-07-05 16:13:00 +0700

    feat(beta-landing): add blueprint for styled + chakra

---
+++ src/containers/BetaLanding/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import {
+  Box,
+  Text
+} from "@chakra-ui/react"
+
+export const Title = styled(Text).attrs({
+
+})``
+
+export const Paragraph = styled(Text).attrs({
+
+})``
