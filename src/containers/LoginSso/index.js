46146b66fa17e00c8fa505d4980d206c4ed0159f
Ari Nugraha <ari.nugraha2318@gmail.com>
2021-07-13 22:41:59 +0700

    feat(component): create back link and title

--- src/containers/LoginSso/index.js
+++ src/containers/LoginSso/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import Bauhaus from 'components/Bauhaus';
+import { Link } from 'react-router-dom';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { Box, Text } from '@chakra-ui/react';
 
-import { Box } from '@chakra-ui/layout';
-
-const LoginSso = () => {
+const LoginSso = ({ history }) => {
   return (
-    <Box>
-    </Box>
+    <>
+      <Bauhaus />
+      <Box width={{lg: '80%', xl: '70%'}}>
+        <Link to='/'>
+          <Text 
+            color="var(--chakra-colors-primary-Purple)" 
+            fontSize="lg" 
+            ml='-9px'
+          >
+              <ChevronLeftIcon w={8} h={8} />
+              Kembali ke Halaman Utama
+          </Text>
+        </Link>
+        <Text 
+          fontSize="3xl" 
+          fontWeight="bold" 
+          mt="4" 
+          textAlign={{sm: 'center', lg: 'left'}}
+        >
+          Daftarkan SSO untuk Update Jadwal
+        </Text>
+      </Box>
+    </>
   );
 };
 
