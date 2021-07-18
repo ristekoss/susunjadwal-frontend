8169f08a199760eb795fe8095e9387bd8b8505ec
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-07-18 23:33:27 +0700

    style(update-matkul): remove trailing spaces & use double quote

--- src/containers/UpdateCourses/index.js
+++ src/containers/UpdateCourses/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
-import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { ChevronLeftIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
   Text,
   useToast
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
-import { postScrapSchedule } from 'services/api';
+import { postScrapSchedule } from "services/api";
 
-import { InputPassword, InputText } from 'components/Forms';
-import { InfoToast, SuccessToast } from 'components/Toast';
-import { Bauhaus } from 'components/Bauhaus';
-import Alert from './Alert';
-import Info from './Info';
-import './styles.css';
+import { InputPassword, InputText } from "components/Forms";
+import { InfoToast, SuccessToast } from "components/Toast";
+import { Bauhaus } from "components/Bauhaus";
+import Alert from "./Alert";
+import Info from "./Info";
+import "./styles.css";
 
 const UpdateCourses = ({ history }) => {
   const isMobile = useSelector((state) => state.appState.isMobile);
@@ -32,22 +33,22 @@ const UpdateCourses = ({ history }) => {
   const onSubmit = async (values) => {
     try {
       InfoToast(
-        'Sedang memperbaharui jadwal',
+        "Sedang memperbaharui jadwal",
         isMobile
       );
       postScrapSchedule(values);
       setTimeout(() => {
         toast.closeAll();
         SuccessToast(
-          'Jadwal berhasil diperbaharui',
+          "Jadwal berhasil diperbaharui",
           isMobile
         );
-        // history.push('/susun')
-        window.location.replace('/susun')
+        // history.push("/susun")
+        window.location.replace("/susun")
       }, 1000);
     } catch (err) {
       InfoToast(
-        'Maaf ada sedikit kendala, silahkan coba beberapa saat lagi',
+        "Maaf ada sedikit kendala, silahkan coba beberapa saat lagi",
         isMobile
       );
       throw err;
@@ -57,29 +58,29 @@ const UpdateCourses = ({ history }) => {
   return (
     <>
       <Bauhaus />
-      <Box width={{lg: '80%', xl: '70%'}}>
-        <Link to='/'>
-          <Text 
-            color="var(--chakra-colors-primary-Purple)" 
-            fontSize="lg" 
-            ml='-9px'
+      <Box width={{lg: "80%", xl: "70%"}}>
+        <Link to="/">
+          <Text
+            color="var(--chakra-colors-primary-Purple)"
+            fontSize="lg"
+            ml="-9px"
           >
               <ChevronLeftIcon w={8} h={8} />
               Kembali ke Halaman Utama
           </Text>
         </Link>
-        <Text 
-          fontSize="3xl" 
-          fontWeight="bold" 
+        <Text
+          fontSize="3xl"
+          fontWeight="bold"
           mt="4"
           mb="20"
-          textAlign={{sm: 'center', lg: 'left'}}
+          textAlign={{sm: "center", lg: "left"}}
         >
           Daftarkan SSO untuk Update Jadwal
         </Text>
         <Info />
         <form className="form-container" onSubmit={handleSubmit(onSubmit)} >
-          <InputText 
+          <InputText
             label="User Name"
             name="username"
             marginTop="1rem"
@@ -109,7 +110,7 @@ const UpdateCourses = ({ history }) => {
             isLoading={isSubmitting}
             type="submit"
             disabled={isSubmitSuccessful}
-            w={{sm: '100%', lg: 'unset'}}
+            w={{sm: "100%", lg: "unset"}}
           >
             Update Jadwal
           </Button>
@@ -120,3 +121,7 @@ const UpdateCourses = ({ history }) => {
 };
 
 export default UpdateCourses;
+
+const FormContainer = styled.div`
+  margin-top: 3rem;
+`
