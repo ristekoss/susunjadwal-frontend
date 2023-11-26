d67dd9098914ae29d8693c6ad33c6bc9f3d6f5d1
Valerian Salim <valerian.salim@ui.ac.id>
2023-11-26 16:09:04 +0700

    remove debug

--- src/hooks/useMixpanel.js
+++ src/hooks/useMixpanel.js
@@ -1,7 +1,6 @@
 import mixpanel from "mixpanel-browser";
 
 mixpanel.init(process.env.REACT_APP_MIXPANEL_PROJECT_TOKEN, {
-  debug: true,
   persistence: "localStorage",
   track_pageview: true,
 });
